536b99631f34e800bbab86ff472648c75a6c7828
Aleksei Mialkin <aleksei@mialkin.ru>
2024-02-06 23:53:41 +0300

    Add new line

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,7 +4,9 @@ import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;const [invoice, customers] = await Promise.all([
+    const id = params.id;
+    
+    const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
